c10cacb270b598db2972a49d88fbf8c774cd63fb
Shohsulton Ummatov <108561261+ShohsultonCode@users.noreply.github.com>
2024-06-23 16:52:43 +0500

    fuc

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,16 @@ import { Route, Routes } from 'react-router-dom'
 import Error from './pages/error'
 import Home from './pages/home'
 import Product from './pages/product'
-import backgroundImage from './assets/ss.jpg'
 
 const App = () => {
-  const divStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    height: '100vh',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  };
+
 
   return (
-    <div style={divStyle}>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/products' element={<Product />}></Route>
         <Route path='*' element={<Error />}></Route>
       </Routes>
-      </div>
   )
 }
 export default App
